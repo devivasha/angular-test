89dfdf2749bdc76a0720963a844165efae9a4c7c
devivasha <nat_ivaniv@ukr.net>
2025-02-19 16:01:48 +0100

    tests fix

--- src/app/pages/course-details/course-details.component.spec.ts
+++ src/app/pages/course-details/course-details.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { of } from 'rxjs';
 
 import { CourseDetailsComponent } from './course-details.component';
 
@@ -8,7 +12,16 @@ describe('CourseDetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CourseDetailsComponent]
+      imports: [CourseDetailsComponent, HttpClientTestingModule, NoopAnimationsModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: of({ get: (key: string) => 'mockId' }),
+            snapshot: { paramMap: { get: () => 'mockId' } }
+          }
+        }
+      ]
     })
     .compileComponents();
 
